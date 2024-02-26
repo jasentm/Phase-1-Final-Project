337aa95fa8a43dd9f7891997d474c34c08ba6de3
Keenan Weise <kcweise@gmail.com>
2024-02-26 09:52:05 -0600

    Added dynamics to space image

--- astronomy.js
+++ astronomy.js
@@ -1,5 +1,6 @@
 //Fetch response from NASA Daily Image
 const fetchMe = () => {
+let currentImg = {}
     fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
     .then(res =>{
     if(res.ok){return res.json();}
@@ -7,23 +8,50 @@ const fetchMe = () => {
       alert("No promise returned");
     }
     })
-    .then(res => renderSpace(res))
-    .then(res => console.log(res))
-    .then(res => renderSpace(res))
+    .then(res => {renderSpace(res); return res})
+    .then(res => currentImg = res)
     .catch(error => console.log(error)) 
 }
+
+const imgSpace = document.getElementById("id-ph3");
+
 //Render space photo 
 const renderSpace = (response) => {
-    
-    const imgSpace = document.getElementById("id-ph3");
-  
+      
     imgSpace.src = response.url;
     imgSpace.alt = `Space photo`;
     imgSpace.title = response.title
+
 };
+
+const hoverRender = (e) => {
+  e.preventDefault()
+
+  imgSpace.classList.remove('detail-image2');
+  imgSpace.classList.add('img-enlarged');
   
-  //Invokes functions
-  const main = () => {
+  
+};
+
+const outRender = (e) => {
+  e.preventDefault()
+
+  imgSpace.classList.remove('img-enlarged');
+  imgSpace.classList.add('detail-image2');
+  
+}
+
+imgSpace.addEventListener("mouseover", (e) => {
+  hoverRender(e);
+})
+
+imgSpace.addEventListener("mouseout", (e) =>{
+  outRender(e);
+})
+ 
+
+//Invokes functions
+const main = () => {
     fetchMe()
   };
 
