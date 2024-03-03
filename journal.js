d7b3b9ac07678dd24e8f403ae143f87db3282c01
Jasen <jasen.miyamoto@gmail.com>
2024-03-03 16:34:17 -0500

    deleted fetch.js, rendered form, rendered journal dates

--- journal.js
+++ journal.js
@@ -1,1 +1,23 @@
+//global variables
+fetch('http://localhost:3000/journals')
+.then(res => {
+    if (res.ok){
+        return res.json()
+    }else {
+        console.error("Something went wrong...")
+    }
+})
+.then((journalData) => {
+    journalData.forEach((journal) => {
+        let entry = document.createElement('div')
+        let journalBoard = document.getElementById('journal-board')
 
+        entry.className = 'entry-box'
+        entry.textContent = journal.date
+
+        journalBoard.appendChild(entry)
+
+
+    })
+})
+.catch((error => console.error(error)))
